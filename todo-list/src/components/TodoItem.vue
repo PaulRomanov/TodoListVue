f05f6e9cac9f1a7816d85072264cf1960ed5308b
PaulRomanov <pavel.ramanau@gmail.com>
2022-11-07 15:09:06 +0400

    start project

---
+++ todo-list/src/components/TodoItem.vue
@@ -0,0 +1,3 @@
+<template>
+  <li>todo item 1</li>
+</template>
---
+++ todo-list/src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+     <TodoItem></TodoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  components: {
+    TodoItem
+  }
+}
+</script>
